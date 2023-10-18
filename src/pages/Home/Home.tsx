96fe3b545c9161d02d4d349439d0e91b4442969b
ricardocisco <richardrulex@gmail.com>
2023-10-18 17:36:14 -0300

    Add Hover

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,15 +3,33 @@ import { FaApple, FaVolumeUp, FaBatteryFull } from "react-icons/fa";
 import { MdOutlineRestore, MdBluetooth, MdWifi } from "react-icons/md";
 import finder from '../../images/finder.png'
 
+
+const PropsIcon = {
+  size: 20,
+}
+
 export default function Home() {
+
+  const diasDaSemana = ['Dom', 'Seg','Ter','Qua','Qui','Sex','Sab']
+  const date = new Date();
+  const dia = date.getDay();
+  const hora = date.getHours();
+  const minutos = date.getMinutes();
+
+  //CONVERSAO STRING
+  const horaString = hora.toString().padStart(2, '0');
+  const minutosString = minutos.toString().padStart(2, '0');
+  const diaString = diasDaSemana[dia]
+ 
+
   return (
     <div className="h-full w-full bg-my-image bg-cover bg-center">
-      <div className="flex flex-col items-center h-full justify-between">
+      <div className="flex flex-col items-center h-full justify-between overflow-hidden">
         <nav className="nav">
           <ul className="ulist">
             <li>
               <a href="/">
-                <FaApple></FaApple>
+                <FaApple {...PropsIcon}></FaApple>
               </a>
             </li>
             <li>
@@ -53,7 +71,7 @@ export default function Home() {
           <ul className="ulist">
             <li>
               <a href="/">
-                <MdOutlineRestore />
+                <MdOutlineRestore {...PropsIcon}/>
               </a>
             </li>
             <li>
@@ -63,33 +81,38 @@ export default function Home() {
             </li>
             <li>
               <a href="">
-                <MdWifi />
+                <MdWifi {...PropsIcon}/>
               </a>
             </li>
             <li>
               <a href="">
-                <FaVolumeUp />
+                <FaVolumeUp {...PropsIcon}/>
               </a>
             </li>
             <li>
               <a href="">
-                <FaBatteryFull />
+                <FaBatteryFull {...PropsIcon}/>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <p>{diaString} {horaString}:{minutosString}</p>
               </a>
             </li>
           </ul>
         </nav>
-        <footer className="flex justify-center bg-neutral-100 bg-opacity-60 rounded-t-lg">
+        <footer className="flex justify-center bg-neutral-100 bg-opacity-60 rounded-t-lg overflow-hidden">
             <div className="flex items-center footer">
                 <img src={finder} alt="finder" className="" />
-                <img src={finder} alt="finder" className="w-[60px]" />
-                <img src={finder} alt="finder" className="w-[60px]" />
-                <img src={finder} alt="finder" className="w-[60px]" />
-                <img src={finder} alt="finder" className="w-[60px]" />
-                <img src={finder} alt="finder" className="w-[60px]" />
-                <img src={finder} alt="finder" className="w-[60px]" />
-                <img src={finder} alt="finder" className="w-[60px]" />
-                <img src={finder} alt="finder" className="w-[60px]" />
-                <img src={finder} alt="finder" className="w-[60px]" />
+                <img src={finder} alt="finder" className="" />
+                <img src={finder} alt="finder" className="" />
+                <img src={finder} alt="finder" className="" />
+                <img src={finder} alt="finder" className="" />
+                <img src={finder} alt="finder" className="" />
+                <img src={finder} alt="finder" className="" />
+                <img src={finder} alt="finder" className="" />
+                <img src={finder} alt="finder" className="" />
+                <img src={finder} alt="finder" className="" />
             </div>
         </footer>
       </div>
